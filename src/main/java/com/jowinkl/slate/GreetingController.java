f2834adb98edc04c075063825f18cd57e2fe044b
Joe Winkler <joe.c.winkler@outlook.com>
2019-08-23 18:21:22 -0400

    Minor formatting changes

--- src/main/java/com/jowinkl/slate/GreetingController.java
+++ src/main/java/com/jowinkl/slate/GreetingController.java
@@ -1,6 +1,7 @@
 package com.jowinkl.slate;
 
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
